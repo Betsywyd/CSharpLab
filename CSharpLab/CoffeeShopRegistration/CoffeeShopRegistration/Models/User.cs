012f983f9fade67fd7af20f8cfa488e773680861
Yiding Wang <yiding.wang82@gmail.com>
2023-05-10 17:39:35 -0400

    coffeeshop

---
+++ CSharpLab/CoffeeShopRegistration/CoffeeShopRegistration/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace CoffeeShopRegistration.Models
+{
+    public enum Gen
+    {
+        Male,
+        Female
+    }
+    public class User
+    {
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string Country { get; set; }
+        public Gen Gender { get; set; }
+        public string UserEmail { get; set; }
+        public string Password { get; set; }
+    }
+}
