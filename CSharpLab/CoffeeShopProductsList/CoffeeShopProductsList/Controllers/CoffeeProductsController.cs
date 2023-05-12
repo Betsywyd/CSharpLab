0d3b7d4a55e0d8673e4339ca5d816dde57404447
Yiding Wang <yiding.wang82@gmail.com>
2023-05-11 23:31:13 -0400

    coffeeproducts

---
+++ CSharpLab/CoffeeShopProductsList/CoffeeShopProductsList/Controllers/CoffeeProductsController.cs
@@ -0,0 +1,35 @@
+﻿using CoffeeShopProductsList.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopProductsList.Controllers
+{
+    public class CoffeeProductsController : Controller
+    {
+        private readonly ILogger<CoffeeProductsController> _logger;
+        private readonly ProductsDbContext _productsDbContext;
+
+        public CoffeeProductsController(ILogger<CoffeeProductsController> logger, ProductsDbContext c)
+        {
+            _logger = logger;
+            _productsDbContext = c;
+        }
+        public IActionResult CoffeeProductsIndex()
+        {
+            return View();
+        }
+
+        public IActionResult CoffeeProductsDetails()
+        {
+            List<Product> result = _productsDbContext.Products.ToList();
+            return View(result);
+        }
+        public IActionResult DisplayDetails(Product e)
+        {
+          
+            return View(e);
+        }
+
+      
+
+    }
+}
---
+++ CSharpLab/CoffeeShopProductsList/CoffeeShopProductsList/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using CoffeeShopProductsList.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopProductsList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ProductsDbContext _productsDbContext;
+
+        public HomeController(ILogger<HomeController> logger, ProductsDbContext c)
+        {
+            _logger = logger;
+            _productsDbContext= c;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            List<Product> result =_productsDbContext.Products.ToList();
+            return View(result);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
