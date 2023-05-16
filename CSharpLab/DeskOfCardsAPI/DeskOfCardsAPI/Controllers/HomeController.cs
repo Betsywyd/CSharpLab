ebb0b5fd199de0172703feac1c2a167e458620c3
Yiding Wang <yiding.wang82@gmail.com>
2023-05-15 21:08:27 -0400

    deskofcards

---
+++ CSharpLab/DeskOfCardsAPI/DeskOfCardsAPI/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using DeskOfCardsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeskOfCardsAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        DeskDAL DeskAPI { get; set; } = new DeskDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Desk d = DeskAPI.GetDesk();
+            return View(d);
+        }
+
+        public IActionResult ShowHand(string id)
+        {
+            Hand h = DeskAPI.GetHand(id);
+            return View(h);
+        }
+
+   
+        public IActionResult CardsDetails(string id,string code)
+        {
+            Images i= DeskAPI.GetHand(id).cards.FirstOrDefault(card => card.code==code).images;
+            return View(i);
+        }
+
+        public IActionResult Privacy()
+        {
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CSharpLab/DeskOfCardsAPI/DeskOfCardsAPI/Models/DeskDAL.cs
@@ -0,0 +1,28 @@
+﻿using RestSharp;
+
+namespace DeskOfCardsAPI.Models
+{
+    public class DeskDAL { 
+    public Desk GetDesk()
+    {
+        string URL = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+        RestClient client = new RestClient(URL);
+        RestRequest request = new RestRequest();
+
+        Desk d = client.Get<Desk>(request);
+
+        return d;
+    }
+
+    public Hand GetHand(string id)
+    {
+        string url = $"https://deckofcardsapi.com/api/deck/{id}/draw/?count=5";
+        RestClient client = new RestClient(url);
+        RestRequest request = new RestRequest();
+
+        Hand h = client.Get<Hand>(request);
+
+        return h;
+    }
+    }
+}
