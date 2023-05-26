bebfddc8b9200be9970e88598104c44386d1e461
Yiding Wang <yiding.wang82@gmail.com>
2023-05-25 23:35:41 -0400

    todoList

--- CSharpLab/Week7/AngularTodo/src/app/app.component.ts
+++ CSharpLab/Week7/AngularTodo/src/app/app.component.ts
@@ -9,14 +9,48 @@ import { Todo} from './todo';
 export class AppComponent {
   title = 'AngularTodo';
 
-todos:Todo[]=[{task:"fold clothes",completed:true},{task:"put clothes in dresser",completed:false},{task:"relax",completed:false},{task:"pet dog",completed:true},{task:"be awesome",completed:true}];
+  showForm:boolean = false;
+todos:Todo[]=[{task:"fold clothes",completed:true},{task:"put clothes in dresser",completed:false},{task:"relax",completed:false},{task:"pet dog",completed:true},{task:"pet cat",completed:false},{task:"be awesome",completed:true}];
+newTodo:Todo={task:"",completed:false};
 
+//deleted:boolean=false;
 
-beCompleted(i){
-  this.
+toComplete(i:number){
+ this.todos[i].completed=true;
 }
 
+toggleForm():void{
+this.showForm=!this.showForm;
+}
 
+//  removeTask(i:number):void{
 
+// for(let j=0;j<this.todos.length;j++){
+//   this.deleted=false;
+// }
+// this.todos.slice(i,1);
+// }
+
+addTodo(){
+ //this.isAdding=true;
+this.todos.push(this.newTodo);
+this.newTodo={task:"",completed:false};
+this.showForm=false;
+}
+
+allCompleted():boolean{
+
+  let count=0;
+  for(let i=0;i<this.todos.length;i++){
+  if(this.todos[i].completed===true){
+    count++;
+  }
+  }
+  if(count===this.todos.length){
+    return true;
+  }
+  return false;
+
+}
 
 }
