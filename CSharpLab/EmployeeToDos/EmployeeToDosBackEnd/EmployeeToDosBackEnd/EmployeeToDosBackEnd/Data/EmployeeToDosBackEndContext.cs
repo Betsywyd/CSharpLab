0b54cb43776d0eb054c183ad1ec744070df75198
Yiding Wang <yiding.wang82@gmail.com>
2023-09-20 10:43:59 -0400

    addBackEnd

---
+++ CSharpLab/EmployeeToDos/EmployeeToDosBackEnd/EmployeeToDosBackEnd/EmployeeToDosBackEnd/Data/EmployeeToDosBackEndContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeToDosBackEnd.Models;
+
+namespace EmployeeToDosBackEnd.Data
+{
+    public class EmployeeToDosBackEndContext : DbContext
+    {
+        public EmployeeToDosBackEndContext (DbContextOptions<EmployeeToDosBackEndContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeToDosBackEnd.Models.Employee> Employee { get; set; } = default!;
+
+        public DbSet<EmployeeToDosBackEnd.Models.ToDo>? ToDo { get; set; }
+    }
+}
